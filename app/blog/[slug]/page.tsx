39ff22e4fc88e3c05fdf65aa09ca1bfa48c58f04
unknown <hung28012002zz@gmail.com>
2025-05-02 16:21:53 +0700

    update

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -9,8 +9,13 @@ export async function generateMetadata({ params }: Params) {
 }
 
 export default function Page({ params }: Params) {
+  const { slug } = params;
+  if (!slug) {  // Handle the case where slug is not provided
+    return <h1>Slug not provided</h1>;  
+  }
+  // You can also handle the case where slug is an empty string or invalid value
   return <>
-  <h1>Slug: {params.slug}</h1> 
+  <h1>Slug: {slug}</h1>  
   <p>TestCase</p>
   <p>Welcome to Ho Chi Minh</p>
   </>;
